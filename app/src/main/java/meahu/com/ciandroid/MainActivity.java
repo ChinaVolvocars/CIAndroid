ed21a845ec84413879df1911a604ea0559815d0e
Tiger <atlantisspeed@gmail.com>
2017-09-28 12:53:39 +0800

    持续构建

--- app/src/main/java/meahu/com/ciandroid/MainActivity.java
+++ app/src/main/java/meahu/com/ciandroid/MainActivity.java
@@ -2,6 +2,8 @@ package meahu.com.ciandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View viewById = findViewById(R.id.tv);
+        viewById.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "构建成功", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
